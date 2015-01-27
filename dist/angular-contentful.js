c320ca8a1f7922865a5226a963a3203a0a2e1526
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-01-27 11:15:36 +0100

    feat(dist): update build

--- dist/angular-contentful.js
+++ dist/angular-contentful.js
@@ -8,6 +8,82 @@
 (function () {
 
   /**
+   * Controller for contentful directives
+   *
+   * By separating the controller we can avoid a lot of
+   * repetitive code and keep the code base as small as
+   * possible.
+   *
+   * @param $attrs
+   * @param contentful
+   * @constructor
+   */
+  function ContentfulDirectiveCtrl($attrs, contentful) {
+
+    var self = this;
+
+    // Placeholder
+    this.entry = null;
+    this.entries = [];
+
+    if ($attrs.contentfulEntry) {
+      contentful
+        .entry($attrs.contentfulEntry)
+        .then(function (response) {
+          self.entry = response.data;
+        });
+    }
+
+    if ($attrs.contentfulEntries) {
+      contentful
+        .entries($attrs.contentfulEntries)
+        .then(function (response) {
+          self.entries = response.data;
+        });
+    }
+
+  }
+
+  // Inject controller dependencies
+  ContentfulDirectiveCtrl.$inject = ['$attrs', 'contentful'];
+
+  // Export
+  angular
+    .module('contentful')
+    .controller('ContentfulDirectiveCtrl', ContentfulDirectiveCtrl);
+
+})();
+
+(function () {
+
+  /**
+   * Directive
+   *
+   * @returns {object} directive definition object
+   */
+  function contentfulEntryDirective() {
+
+    return {
+      restrict: 'EA',
+      controller: 'ContentfulDirectiveCtrl',
+      controllerAs: '$contentfulEntry'
+    };
+
+  }
+
+  // Inject directive dependencies
+  contentfulEntryDirective.$inject = [];
+
+  // Export
+  angular
+    .module('contentful')
+    .directive('contentfulEntry', contentfulEntryDirective);
+
+})();
+
+(function () {
+
+  /**
    * Contentful service provider
    */
   function contentfulProvider() {
