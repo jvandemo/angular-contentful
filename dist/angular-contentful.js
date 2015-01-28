0300711cd87c64071828bbf5cb0fa5a819730579
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-01-28 19:16:22 +0100

    feat(dist): update build

--- dist/angular-contentful.js
+++ dist/angular-contentful.js
@@ -18,7 +18,7 @@
    * @param contentful
    * @constructor
    */
-  function ContentfulDirectiveCtrl($attrs, contentful) {
+  function ContentfulDirectiveCtrl($attrs, contentful, contentfulHelpers) {
 
     var self = this;
 
@@ -40,14 +40,14 @@
       contentful
         .entries($attrs.contentfulEntries)
         .then(function (response) {
-          self.entries = response.data;
+          self.entries = contentfulHelpers.resolveResponse(response.data);
         });
     }
 
   }
 
   // Inject controller dependencies
-  ContentfulDirectiveCtrl.$inject = ['$attrs', 'contentful'];
+  ContentfulDirectiveCtrl.$inject = ['$attrs', 'contentful', 'contentfulHelpers'];
 
   // Export
   angular
@@ -109,6 +109,80 @@
   angular
     .module('contentful')
     .directive('contentfulEntry', contentfulEntryDirective);
+
+})();
+
+(function () {
+
+  function contentfulHelpersFactory() {
+
+    function ContentfulHelpers() {
+    }
+
+    ContentfulHelpers.prototype.resolveResponse = function resolveResponse(response) {
+      var self = this;
+      self.walkMutate(response, self.isLink, function (link) {
+        return self.getLink(response, link) || link;
+      });
+      return response.items || [];
+    };
+
+    ContentfulHelpers.prototype.isLink = function isLink(object) {
+      if (object && object.sys && object.sys.type && object.sys.type === 'Link') {
+        return true;
+      }
+      return false;
+    };
+
+    ContentfulHelpers.prototype.getLink = function getLink(response, link) {
+      var self = this;
+      var type = link.sys.linkType;
+      var id = link.sys.id;
+      var pred = function (resource) {
+        return resource && resource.sys && resource.sys.type === type && resource.sys.id === id;
+      };
+      return self.findLink(response.items, pred) ||
+        response.includes && self.findLink(response.includes[type], pred);
+    };
+
+    ContentfulHelpers.prototype.findLink = function findLink(arr, pred) {
+      var i;
+      var link = null;
+      if (!angular.isArray(arr)) {
+        return link;
+      }
+      for (i = 0; i < arr.length; i++) {
+        if (pred(arr[i])) {
+          link = arr[i];
+          break;
+        }
+      }
+      return link;
+    };
+
+    ContentfulHelpers.prototype.walkMutate = function walkMutate(input, pred, mutator) {
+      var self = this;
+      if (pred(input)){
+        return mutator(input);
+      }
+
+      if (angular.isArray(input) || angular.isObject(input)) {
+        angular.forEach(input, function (item, key) {
+          input[key] = self.walkMutate(item, pred, mutator);
+        });
+        return input;
+      }
+      return input;
+    };
+
+    return new ContentfulHelpers();
+
+  }
+
+  // Export
+  angular
+    .module('contentful')
+    .factory('contentfulHelpers', contentfulHelpersFactory);
 
 })();
 
