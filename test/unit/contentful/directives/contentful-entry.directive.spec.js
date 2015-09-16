725f76fc06b6e8e52c0c5fa4a1c0cae309d772b1
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-09-16 04:29:50 +0200

    test(contentful-entry): update fakeModule definition

--- test/unit/contentful/directives/contentful-entry.directive.spec.js
+++ test/unit/contentful/directives/contentful-entry.directive.spec.js
@@ -19,8 +19,7 @@ describe('contentfulEntry directive', function () {
 
     // Define a fake module so we can configure the provider
     // before injecting the service
-    var fakeModule = angular.module('fakeModule', function () {
-    });
+    var fakeModule = angular.module('fakeModule', []);
     fakeModule.config(function (_contentfulProvider_) {
       contentfulProvider = _contentfulProvider_;
       contentfulProvider.setOptions(customOptions);
